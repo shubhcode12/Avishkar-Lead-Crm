89685cf0ef2ca9e809d721d571151a884d87c43b
shubhcode <shubhcode.dev@gmail.com>
2024-08-26 21:28:51 +0530

    first commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,12 @@
+var configz = {
+  apiKey: "AIzaSyBWec8Jzy0jLha15pKKmsPd4KWPrCn-VfA",
+  authDomain: "avishkar-infra-crm.firebaseapp.com",
+  databaseURL: "https://avishkar-infra-crm-default-rtdb.firebaseio.com",
+  projectId: "avishkar-infra-crm",
+  storageBucket: "avishkar-infra-crm.appspot.com",
+  messagingSenderId: "792194537199",
+  appId: "1:792194537199:web:ff86961d0f8552a7096d1a",
+  measurementId: "G-175BJ33Z56",
+};
+
+firebase.initializeApp(configz);
